1bef4924fd24f2c14dc19d03ad372ab9b151300f
ninh2k <ninh2k@uw.edu>
2018-01-11 07:29:57 -0600

    update BitPenny bitpenny

--- src/qt/locale/bitcoin_bs.ts
+++ src/qt/locale/bitcoin_bs.ts
@@ -14,8 +14,8 @@
 <context>
     <name>BitcoinGUI</name>
     <message>
-        <source>Litecoin</source>
-        <translation>Litecoin</translation>
+        <source>BitPenny</source>
+        <translation>BitPenny</translation>
     </message>
     </context>
 <context>
@@ -156,8 +156,8 @@
 <context>
     <name>bitcoin-core</name>
     <message>
-        <source>Litecoin Core</source>
-        <translation>Litecoin Jezrga</translation>
+        <source>BitPenny Core</source>
+        <translation>BitPenny Jezrga</translation>
     </message>
     </context>
 </TS>
